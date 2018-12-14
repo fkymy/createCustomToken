5707ced4fb2efdaad1a44d4fad366610c876d9f4
Yuske Fukuyama <yuskefuku@gmail.com>
2018-12-14 15:10:23 +0900

    fix a little

--- functions/lib/createCustomToken.js
+++ functions/lib/createCustomToken.js
@@ -3,58 +3,131 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const rp = require("request-promise");
-const config = require('../config.json');
+// Be sure to set lineChannelid to firebase config.
+// e.g. firebase functions:config:set line.channelid=XXXXXXXXXX
+const lineChannelid = functions.config().line.channelid;
 const runtimeOpts = {
     timeoutSeconds: 300,
-    memory: '2GB'
 };
-exports.createCustomToken = functions.runWith(runtimeOpts).https
+const baseUrl = 'https://api.line.me';
+function generateLineApiRequest(path, lineAccessToken) {
+    return {
+        url: baseUrl + path,
+        headers: {
+            'Authorization': `Bearer ${lineAccessToken}`
+        },
+        json: true,
+    };
+}
+exports.testBasicCreateCustomToken = functions.runWith(runtimeOpts).https
     .onRequest((request, response) => {
-    if (request.body.token === undefined) {
+    console.info("request.body", request.body);
+    console.info("request.body.access_token", request.body.access_token);
+    const ret = {
+        customToken: "helloShittyAPIthisIsMyFirebaseCustomToken",
+    };
+    return response.status(200).send(ret);
+});
+exports.testIntermediateCreateCustomToken = functions.runWith(runtimeOpts).https
+    .onRequest((request, response) => {
+    console.info("request.body", request.body);
+    const token = request.body.access_token;
+    if (token === undefined) {
         const ret = {
-            error_message: 'AccessToken not found',
+            error_message: 'access_token not found in request',
         };
         return response.status(400).send(ret);
     }
-    return verifyLineToken(request.body)
-        .then((customAuthToken) => {
+    return verifyLineToken(token)
+        .then((verifyLineTokenResponse) => {
+        console.info('verifyLineTokenResponse', verifyLineTokenResponse);
+        return fetchLineProfile(token);
+    })
+        .then((fetchLineProfileResponse) => {
+        console.info('fetchLineProfileResponse', fetchLineProfileResponse);
         const ret = {
-            firebase_token: customAuthToken
+            customToken: "helloShittyAPIthisIsMyFirebaseCustomToken",
         };
         return response.status(200).send(ret);
     }).catch((err) => {
+        console.error(err);
         const ret = {
             error_message: `Authentication error: ${err}`,
         };
-        return response.status(200).send(ret);
+        return response.status(403).send(ret);
     });
 });
-function verifyLineToken(requestBody) {
+exports.createCustomToken = functions.runWith(runtimeOpts).https
+    .onRequest((request, response) => {
+    console.info("request.body", request.body);
+    const token = request.body.access_token;
+    if (token === undefined) {
+        const ret = {
+            error_message: 'access_token not found in request',
+        };
+        return response.status(400).send(ret);
+    }
+    return verifyLineToken(token)
+        .then((verifyLineTokenResponse) => {
+        console.info('verifyLineTokenResponse', verifyLineTokenResponse);
+        return fetchLineProfile(token);
+    })
+        .then((fetchLineProfileResponse) => {
+        console.info('fetchLineProfileResponse', fetchLineProfileResponse);
+        return connectToFirebaseUser(fetchLineProfileResponse);
+    })
+        .then((userRecord) => {
+        console.info('userRecord', userRecord);
+        return admin.auth().createCustomToken(userRecord.uid);
+    })
+        .then((customToken) => {
+        const ret = {
+            customToken: customToken
+        };
+        return response.status(200).send(ret);
+    }).catch((err) => {
+        console.error(err);
+        const ret = {
+            error_message: `Authentication error: ${err}`,
+        };
+        return response.status(403).send(ret);
+    });
+});
+function verifyLineToken(token) {
     return rp({
         method: 'GET',
-        uri: `https://api.line.me/oauth2/v2.1/verify?access_token=${requestBody.token}`,
-        json: true,
+        uri: `https://api.line.me/oauth2/v2.1/verify?access_token=${token}`,
+        json: true
     }).then((response) => {
-        if (response.client_id !== config.line.channelid) {
-            return Promise.reject(new Error('LINE channel ID mismatched'));
+        if (response.client_id !== lineChannelid) {
+            return Promise.reject(new Error('The provided access token and service LINE channel ID mismatched'));
         }
-        return getFirebaseUser(requestBody);
-    }).then((userRecord) => {
-        return admin.auth().createCustomToken(userRecord.uid);
-    }).then((token) => {
-        return token;
+        return response;
     });
 }
-function getFirebaseUser(requestBody) {
-    const firebaseUid = `line:${requestBody.id}`;
+function fetchLineProfile(token) {
+    const getProfileOptions = generateLineApiRequest('/v2/profile', token);
+    return rp(getProfileOptions);
+}
+function connectToFirebaseUser(fetchLineProfileResponse) {
+    if (fetchLineProfileResponse.userId === undefined) {
+        return Promise.reject(new Error('LINE profile response does not contain userId'));
+    }
+    const firebaseUid = `${fetchLineProfileResponse.userId}`;
     return admin.auth().getUser(firebaseUid).then(function (userRecord) {
         return userRecord;
     }).catch((err) => {
         if (err.code === 'auth/user-not-found') {
-            return admin.auth().createUser({
-                uid: firebaseUid,
-                displayName: requestBody.name,
-            });
+            const user = {
+                uid: firebaseUid
+            };
+            if (fetchLineProfileResponse.hasOwnProperty('displayName')) {
+                user['displayName'] = fetchLineProfileResponse.displayName;
+            }
+            if (fetchLineProfileResponse.hasOwnProperty('pictureUrl')) {
+                user['photoURL'] = fetchLineProfileResponse.pictureUrl;
+            }
+            return admin.auth().createUser(user);
         }
         return Promise.reject(err);
     });
--- functions/src/createCustomToken.ts
+++ functions/src/createCustomToken.ts
@@ -2,63 +2,144 @@ import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import * as rp from 'request-promise'
 
-const config = require('../config.json');
+// Be sure to set lineChannelid to firebase config.
+// e.g. firebase functions:config:set line.channelid=XXXXXXXXXX
+const lineChannelid = functions.config().line.channelid
 
 const runtimeOpts = {
     timeoutSeconds: 300,
-    memory: '2GB'
 }
 
-export const createCustomToken = functions.runWith(runtimeOpts).https
+const baseUrl = 'https://api.line.me'
+function generateLineApiRequest(path: String, lineAccessToken: String) {
+    return {
+        url: baseUrl + path,
+        headers: {
+            'Authorization': `Bearer ${lineAccessToken}`
+        },
+        json: true,
+    }
+}
+
+export const testBasicCreateCustomToken = functions.runWith(runtimeOpts).https
     .onRequest((request, response) => {
-        if (request.body.token === undefined) {
+        console.info("request.body", request.body)
+        const ret = {
+            customToken: "helloShittyAPIthisIsMyFirebaseCustomToken",
+        }
+        return response.status(200).send(ret)
+    })
+
+export const testIntermediateCreateCustomToken = functions.runWith(runtimeOpts).https
+    .onRequest((request, response) => {
+        console.info("request.body", request.body)
+        const token = request.body.access_token
+        if (token === undefined) {
             const ret = {
-                error_message: 'AccessToken not found',
+                error_message: 'access_token not found in request',
             }
             return response.status(400).send(ret)
         }
 
-        return verifyLineToken(request.body)
-            .then((customAuthToken) => {
+        return verifyLineToken(token)
+            .then((verifyLineTokenResponse) => {
+                console.info('verifyLineTokenResponse', verifyLineTokenResponse)
+                return fetchLineProfile(token)
+            })
+            .then((fetchLineProfileResponse) => {
+                console.info('fetchLineProfileResponse', fetchLineProfileResponse)
                 const ret = {
-                    firebase_token: customAuthToken
+                    customToken: "helloShittyAPIthisIsMyFirebaseCustomToken",
                 }
                 return response.status(200).send(ret)
             }).catch((err) => {
+                console.error(err)
                 const ret = {
                     error_message: `Authentication error: ${err}`,
                 }
-                return response.status(200).send(ret)
+                return response.status(403).send(ret)
             })
     })
 
-function verifyLineToken(requestBody) {
+export const createCustomToken = functions.runWith(runtimeOpts).https
+    .onRequest((request, response) => {
+        console.info("request.body", request.body)
+
+        const token = request.body.access_token
+        if (token === undefined) {
+            const ret = {
+                error_message: 'access_token not found in request',
+            }
+            return response.status(400).send(ret)
+        }
+
+        return verifyLineToken(token)
+            .then((verifyLineTokenResponse) => {
+                console.info('verifyLineTokenResponse', verifyLineTokenResponse)
+                return fetchLineProfile(token)
+            })
+            .then((fetchLineProfileResponse) => {
+                console.info('fetchLineProfileResponse', fetchLineProfileResponse)
+                return connectToFirebaseUser(fetchLineProfileResponse)
+            })
+            .then((userRecord) => {
+                console.info('userRecord', userRecord)
+                return admin.auth().createCustomToken(userRecord.uid)
+            })
+            .then((customToken) => {
+                const ret = {
+                    customToken: customToken
+                }
+                return response.status(200).send(ret)
+            }).catch((err) => {
+                console.error(err)
+                const ret = {
+                    error_message: `Authentication error: ${err}`,
+                }
+                return response.status(403).send(ret)
+            })
+    })
+
+function verifyLineToken(token) {
     return rp({
         method: 'GET',
-        uri: `https://api.line.me/oauth2/v2.1/verify?access_token=${requestBody.token}`,
-        json: true,
+        uri: `https://api.line.me/oauth2/v2.1/verify?access_token=${token}`,
+        json: true
     }).then((response) => {
-        if (response.client_id !== config.line.channelid) {
-            return Promise.reject(new Error('LINE channel ID mismatched'))
+        if (response.client_id !== lineChannelid) {
+            return Promise.reject(new Error('The provided access token and service LINE channel ID mismatched'))
         }
-        return getFirebaseUser(requestBody)
-    }).then((userRecord) => {
-        return admin.auth().createCustomToken(userRecord.uid)
-    }).then((token) => {
-        return token
+        return response
     })
 }
 
-function getFirebaseUser(requestBody) {
-    const firebaseUid = `line:${requestBody.id}`
+function fetchLineProfile(token) {
+    const getProfileOptions = generateLineApiRequest('/v2/profile', token)
+    return rp(getProfileOptions)
+}
+
+function connectToFirebaseUser(fetchLineProfileResponse) {
+    if (fetchLineProfileResponse.userId === undefined) {
+        return Promise.reject(new Error('LINE profile response does not contain userId'))
+    }
+
+    const firebaseUid = `${fetchLineProfileResponse.userId}`
+
     return admin.auth().getUser(firebaseUid).then(function(userRecord) {
         return userRecord
     }).catch((err) => {
         if (err.code === 'auth/user-not-found') {
-            return admin.auth().createUser({
-                uid: firebaseUid,
-                displayName: requestBody.name,
-            })
+            const user = {
+                uid: firebaseUid
+            }
+            if (fetchLineProfileResponse.hasOwnProperty('displayName')) {
+                user['displayName'] = fetchLineProfileResponse.displayName
+            }
+            if (fetchLineProfileResponse.hasOwnProperty('pictureUrl')) {
+                user['photoURL'] = fetchLineProfileResponse.pictureUrl
+            }
+
+            return admin.auth().createUser(user)
         }
         return Promise.reject(err)
     })
